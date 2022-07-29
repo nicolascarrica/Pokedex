ef30bd97d3151d0dad48a4af135ba3e964e38d4c
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2022-07-29 09:45:22 -0300

    Agrega correcciones
    Se hicieron correcciones en el codigo para mostrar con difrentes colores los diferentes tipos de pokemon seleccionados

--- src/pokedex.js
+++ src/pokedex.js
@@ -1,9 +1,27 @@
 // https://pokeapi.co/api/v2/pokemon/ditto
-
+const coloresParaTipos = {
+  electric: '#FFEA70',
+  normal: '#B09398',
+  fire: '#FF675C',
+  water: '#0596C7',
+  ice: '#AFEAFD',
+  rock: '#999799',
+  flying: '#7AE7C7',
+  grass: '#4A9681',
+  psychic: '#FFC6D9',
+  ghost: '#561D25',
+  bug: '#A2FAA3',
+  poison: '#795663',
+  ground: '#D2B074',
+  dragon: '#DA627D',
+  steel: '#1D8A99',
+  fighting: '#2F2F2F',
+  default: '#2A1A1F',
+};
 
 const URLAPI = 'https://pokeapi.co/api/v2/pokemon'
 const offset = 0
-const limit = 202
+const limit = 150
 
 function agregarPokemon(){
     
@@ -11,14 +29,11 @@ function agregarPokemon(){
     .then(resultado => resultado.json())
     .then((datos) => {
         console.log(datos);
-
         Object.keys(datos.results).forEach(pokemon => {
-  
-          // console.log(response.results[pokemon].name);
-          let pokemones = datos.results[pokemon].name;
-        
-          listadoPokemones(pokemones);
           
+          let pokemones = datos.results[pokemon].name;      
+          listadoPokemones(pokemones);
+                
         })
       })  
 }
@@ -76,10 +91,13 @@ function informacionTipoPokemones(datos){
   const tiposPokemon = document.querySelector('#poke-tipo');
   tiposPokemon.innerHTML = '';
   const tipos = datos.types
+  // console.log(tipos[0].type.name)
   tipos.forEach(type => {
-    const textoParaTipo = document.createElement('div');
-    textoParaTipo.textContent = type.type.name.toUpperCase();
-    tiposPokemon.appendChild(textoParaTipo); 
+    const contenedorParaTipo = document.createElement('div');
+    contenedorParaTipo.textContent = type.type.name.toUpperCase();
+    contenedorParaTipo.style.backgroundColor = coloresParaTipos[type.type.name]
+    tiposPokemon.appendChild(contenedorParaTipo); 
+  
   })
 
 }
@@ -94,7 +112,7 @@ function informacionEstadisticasPokemones(datos){
     const elementoCantidadEstadistica = document.createElement('div');
     const elementoNombreEstadistica = document.createElement('div');
 
-    elementoNombreEstadistica.textContent = stat.stat.name;
+    elementoNombreEstadistica.textContent = stat.stat.name.toUpperCase();
     elementoCantidadEstadistica.textContent = stat.base_stat;
 
     elementoEstadistica.appendChild(elementoNombreEstadistica);
@@ -107,9 +125,5 @@ function informacionEstadisticasPokemones(datos){
 
 }
  
-
-
-  
-
 
 
